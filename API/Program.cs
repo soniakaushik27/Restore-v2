699771322302d24f12ed3f8c62d4cdc45d740d7c
soniakaushik27 <154929919+soniakaushik27@users.noreply.github.com>
2025-09-25 02:22:49 +0530

    End Of Section 2

---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt=>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.MapControllers();
+DbInitializer.InitDb(app);
+
+app.Run();
