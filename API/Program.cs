15d91206c81b3a5a15dc977714faa81f537bb120
soniakaushik27 <154929919+soniakaushik27@users.noreply.github.com>
2025-09-27 01:19:09 +0530

    End Of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,12 +11,15 @@ builder.Services.AddDbContext<StoreContext>(opt=>{
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 app.MapControllers();
 DbInitializer.InitDb(app);
 
